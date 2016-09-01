0741a5a718709aa5612b2f2e7e1b3f87f38e3ab2
Nidhi Saini <NidhiSaini@nidhis-mbp.gateway.pace.com>
2016-09-01 12:51:01 -0700

    first commit

---
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeApplication.java
@@ -0,0 +1,10 @@
+package com.codepath.android.themeswitcher.app;
+
+import android.app.Application;
+
+
+public class ThemeApplication extends Application {
+    // App level variable to retain selected spinner value
+    public static int currentPosition;
+
+}
